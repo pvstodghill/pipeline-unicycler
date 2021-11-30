db94dd506359084898e685a64b840acf9b3add59
Paul Stodghill <paul.stodghill@usda.gov>
2021-11-30 06:02:17 -0500

    doit01.bash : use config vars instead of file existence tests.

--- doit01.bash
+++ doit01.bash
@@ -8,16 +8,17 @@
 
 rm -rf ${FILTLONG}
 
-if [ -e ${INPUTS}/raw_nanopore.fastq.gz ] ; then
+if [ -z "${NANOPORE_FQ_GZ}" ] ; then
 
+    echo 1>&2 '# No long reads. Skipping filtlong...'
+
+else
     mkdir -p ${FILTLONG}
     echo 1>&2 '# Running filtlong...'
     ${HOWTO} filtlong --min_length 1000 --keep_percent 95 \
 	     ${INPUTS}/raw_nanopore.fastq.gz \
 	| gzip > ${FILTLONG}/filtered_nanopore.fastq.gz
 
-else
-    echo 1>&2 '# No long reads. Skipping filtlong...'
 fi
 
 # ------------------------------------------------------------------------
