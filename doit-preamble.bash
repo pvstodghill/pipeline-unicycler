9671e785dc1f5c85c43ca0311e29a6e704943859
Paul Stodghill <paul.stodghill@usda.gov>
2021-11-30 05:52:48 -0500

    Add doit00.bash

---
+++ doit-preamble.bash
@@ -0,0 +1,78 @@
+#! /bin/bash
+
+HOWTO="./scripts/howto -q -T data/tmp -f packages.yaml"
+THREADS=$(nproc --all)
+
+export LC_ALL=C
+
+# ------------------------------------------------------------------------
+
+. config.bash
+
+# ------------------------------------------------------------------------
+
+if [ "$PACKAGES_FROM" = conda ] ; then
+    if [ -z "$CONDA_EXE" ] ; then
+	CONDA_EXE=$(type -p conda)
+    fi
+fi
+
+# In order to help test portability, I eliminate all of my
+# personalizations from the PATH, etc.
+if [ "$PVSE" ] ; then
+    export PATH=/usr/local/bin:/usr/bin:/bin
+    export PERL5LIB=
+    export PERL_LOCAL_LIB_ROOT=
+    export PERL_MB_OPT=
+    export PERL_MM_OPT=
+    export PYTHONPATH=
+fi
+
+case X"$PACKAGES_FROM"X in
+    XcondaX)
+	CONDA_PREFIX=$(dirname $(dirname $CONDA_EXE))
+	. "${CONDA_PREFIX}/etc/profile.d/conda.sh"
+	conda activate $CONDA_ENV
+
+	;;
+    XhowtoX|XstubsX)
+	export PATH=$(realpath $(dirname ${BASH_SOURCE[0]}))/stubs:"$PATH"
+	;;
+    XnativeX)
+	: nothing
+	;;
+    XX)
+	echo 1>&2 "\$PACKAGES_FROM is not set"
+	exit 1
+	;;
+    X*X)
+	echo 1>&2 "\$PACKAGES_FROM is recognized: $PACKAGES_FROM"
+	exit 1
+	;;
+    *)
+	echo 1>&2 "Cannot happen"
+	exit 1
+esac
+
+# ------------------------------------------------------------------------
+
+if [ -z "$PARALLEL_CMD" ] ; then
+    PARALLEL_CMD="$(type -p parallel)"
+fi
+
+function run_commands {
+    if [ "$PARALLEL_CMD" ] ; then
+	eval $PARALLEL_CMD -j ${THREADS} -kv
+    else
+	bash -x
+    fi
+}
+
+# ------------------------------------------------------------------------
+
+set -e
+set -o pipefail
+
+# ------------------------------------------------------------------------
+
+INPUTS=data/00_inputs
---
+++ doit00.bash
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+. doit-preamble.bash
+
+# ------------------------------------------------------------------------
+# Step 0. Set up
+# ------------------------------------------------------------------------
+
+if [ -d data ] ; then
+    echo 1>&2 "data/ already exists. cannot continue."
+    exit 1
+fi
+
+echo 1>&2 '# Initializing data/...'
+mkdir -p data/tmp
+mkdir -p ${INPUTS}
+
+echo 1>&2 '# Making copies of raw reads...'
+
+if [ "${NANOPORE_FQ_GZ}" ] ; then
+    OK=1
+    cp ${NANOPORE_FQ_GZ} ${INPUTS}/raw_nanopore.fastq.gz
+fi
+if [ "${R1_FQ_GZ}" ] ; then
+    OK=1
+    cp ${R1_FQ_GZ} ${INPUTS}/raw_short_R1.fastq.gz
+    cp ${R2_FQ_GZ} ${INPUTS}/raw_short_R2.fastq.gz
+fi
+
+if [ -z "$OK" ] ; then
+    echo 1>&2 "Error: either long or short reads are required. both can be provided."
+    exit 1
+fi
+
+# ------------------------------------------------------------------------
+# Done.
+# ------------------------------------------------------------------------
+
+echo 1>&2 ''
+echo 1>&2 '# Done.'
