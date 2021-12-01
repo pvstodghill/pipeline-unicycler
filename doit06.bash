dee3f66a5e79d81485b16516c6ae52618bb01497
Paul Stodghill <paul.stodghill@usda.gov>
2021-11-30 19:53:52 -0500

    Fixes after first run.
    - Make sure that GENUS and SPECIES are set.
    - Don't bother "rm -f data/assembly.fasta"
    - "sanitized" -> "normalized"

--- doit06.bash
+++ doit06.bash
@@ -2,6 +2,16 @@
 
 . doit-preamble.bash
 
+if [ -z "${GENUS}" -o "${GENUS}" = FIXME ] ; then
+    echo 1>&2 config.bash: GENUS is not set.
+    exit 1
+fi
+
+if [ -z "${SPECIES}" -o "${SPECIES}" = FIXME ] ; then
+    echo 1>&2 config.bash: SPECIES is not set.
+    exit 1
+fi
+
 # ------------------------------------------------------------------------
 # Step 6. Running PGAP or Prokka
 # ------------------------------------------------------------------------
@@ -31,8 +41,6 @@ if [ "$PGAP_HOME" ] ; then
 
     echo 1>&2 '# Finishing up...'
 
-    rm -f data/assembly.fasta
-
     cp ${PGAP_OUT}/annot.faa data/final.faa
     cp ${PGAP_OUT}/annot.fna data/final.fna
     cp ${PGAP_OUT}/annot.gbk data/final.gbk
@@ -52,11 +60,9 @@ else
 	     --strain ${STRAIN} \
 	     --locustag ${STRAIN}_prokka \
 	     --rfam --addgenes \
-	     ${SANITIZED}/unsanitized.fasta
+	     ${NORMALIZED}/normalized.fasta
 
     echo 1>&2 '# Finishing up...'
-
-    rm -f data/assembly.fasta
 
     cp ${PROKKA}/output.faa data/final.faa
     cp ${PROKKA}/output.fna data/final.fna
